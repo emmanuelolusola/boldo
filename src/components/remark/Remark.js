82cf0bcfa10a57d6b453c673d62cd12c07cf19aa
emmanuelolusola <emmanuelolusolaoyeleke@gmail.com>
2023-05-01 23:36:19 +0100

    remark-updated

--- src/components/remark/Remark.js
+++ src/components/remark/Remark.js
@@ -3,6 +3,7 @@ import React from "react";
 import "./remark.css";
 
 import pic1 from "../../assets/pic1.svg";
+import pic2 from "../../assets/pic2.svg";
 import check from "../../assets/check.svg";
 
 const Remark = () => {
@@ -30,6 +31,30 @@ const Remark = () => {
             <p className="r-p">Business-to-consumer long tail.</p>
           </div>
           <button className="r-button">Start now</button>
+        </div>
+      </div>
+      <div className="remark-bottom">
+        <div className="r-right">
+          <p className="r-header">
+            We connect our customers with the best, and help them keep up-and
+            stay open.
+          </p>
+          <div className="r-paragraph">
+            <img src={check} alt="check" />
+            <p className="r-p">We connect our customers with the best.</p>
+          </div>
+          <div className="r-paragraph">
+            <img src={check} alt="check" />
+            <p className="r-p">Advisor success customer launch party.</p>
+          </div>
+          <div className="r-paragraph">
+            <img src={check} alt="check" />
+            <p className="r-p">Business-to-consumer long tail.</p>
+          </div>
+          <button className="r-button">Start now</button>
+        </div>
+        <div className="r-left">
+          <img src={pic2} alt="remark" />
         </div>
       </div>
       <div className="remark-bottom"></div>
