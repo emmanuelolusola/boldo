16e36d3676f9e08718256dff7a0604cf6651f36c
emmanuelolusola <emmanuelolusolaoyeleke@gmail.com>
2023-05-01 23:49:05 +0100

    remark: updated

--- src/components/remark/Remark.js
+++ src/components/remark/Remark.js
@@ -4,6 +4,9 @@ import "./remark.css";
 
 import pic1 from "../../assets/pic1.svg";
 import pic2 from "../../assets/pic2.svg";
+import leaf from "../../assets/leaf.svg";
+import eye from "../../assets/eye.svg";
+import sun from "../../assets/sun.svg";
 import check from "../../assets/check.svg";
 
 const Remark = () => {
@@ -39,16 +42,16 @@ const Remark = () => {
             We connect our customers with the best, and help them keep up-and
             stay open.
           </p>
-          <div className="r-paragraph">
-            <img src={check} alt="check" />
+          <div className="r-paragraph-2">
+            <img src={leaf} alt="check" />
             <p className="r-p">We connect our customers with the best.</p>
           </div>
-          <div className="r-paragraph">
-            <img src={check} alt="check" />
+          <div className="r-paragraph-2">
+            <img src={eye} alt="check" />
             <p className="r-p">Advisor success customer launch party.</p>
           </div>
-          <div className="r-paragraph">
-            <img src={check} alt="check" />
+          <div className="r-paragraph-2">
+            <img src={sun} alt="check" />
             <p className="r-p">Business-to-consumer long tail.</p>
           </div>
           <button className="r-button">Start now</button>
