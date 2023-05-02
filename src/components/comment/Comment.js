e0d7761804f6095d0d46f735e4409a4851be9114
emmanuelolusola <emmanuelolusolaoyeleke@gmail.com>
2023-05-02 11:16:09 +0100

    comment section

--- src/components/comment/Comment.js
+++ src/components/comment/Comment.js
@@ -5,7 +5,15 @@ import "./comment.css";
 const Comment = () => {
   return (
     <div className="comment-main">
-      <div className="comment-container"></div>
+      <div className="comment-container">
+        <p className="comment-header">
+          An enterprise template to ramp up your company website
+        </p>
+        <div className="comment-input">
+          <input type="text" placeholder="Your email address" />
+          <button className="comment-button">Start now</button>
+        </div>
+      </div>
     </div>
   );
 };
