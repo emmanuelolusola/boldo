e21b5c87a7ad3b04f1827ca5a03b5909a0ca5c3c
emmanuelolusola <emmanuelolusolaoyeleke@gmail.com>
2023-05-03 23:08:23 +0100

    connect section

--- src/components/connect/Connect.js
+++ src/components/connect/Connect.js
@@ -3,6 +3,8 @@ import React from "react";
 import arrowLeft from "../../assets/round-arrow-left.svg";
 import arrowRight from "../../assets/round-arrow-right.svg";
 import albus from "../../assets/albus.png";
+import severus from "../../assets/severus.png";
+import harry from "../../assets/harry.png";
 
 const Connect = () => {
   return (
@@ -19,15 +21,43 @@ const Connect = () => {
         </div>
         <div className="bottom">
           <div className="bottom-container">
-            <h1 className="bottom-p">
+            <p className="bottom-p">
               “Buyer buzz partner network disruptive non-disclosure agreement
               business”
-            </h1>
+            </p>
             <div className="bottom-profile">
               <img src={albus} alt="profile" />
               <div className="bottom-profile-sec">
                 <p className="bottom-profile-name">Albus Dumbledore</p>
                 <p className="bottom-profile-position">Manager @ Howarts</p>
+              </div>
+            </div>
+          </div>
+          <div className="bottom-container">
+            <p className="bottom-p">
+              “Learning curve infrastructure value proposition advisor strategy
+              user experience hypotheses investor.”
+            </p>
+            <div className="bottom-profile">
+              <img src={severus} alt="profile" />
+              <div className="bottom-profile-sec">
+                <p className="bottom-profile-name">Severus Snape</p>
+                <p className="bottom-profile-position">Manager @ Slytherin</p>
+              </div>
+            </div>
+          </div>
+          <div className="bottom-container">
+            <p className="bottom-p">
+              “Release facebook responsive web design business model canvas seed
+              money monetization.”
+            </p>
+            <div className="bottom-profile">
+              <img src={harry} alt="profile" />
+              <div className="bottom-profile-sec">
+                <p className="bottom-profile-name">Harry Potter</p>
+                <p className="bottom-profile-position">
+                  Team Leader @ Gryffindor
+                </p>
               </div>
             </div>
           </div>
