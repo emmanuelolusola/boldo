4f2432300bb18579d98d69cb364a6a2309c16661
emmanuelolusola <emmanuelolusolaoyeleke@gmail.com>
2023-05-03 22:38:37 +0100

    connect section

--- src/components/connect/Connect.js
+++ src/components/connect/Connect.js
@@ -1,7 +1,25 @@
 import React from "react";
 
+import arrowLeft from "../../assets/round-arrow-left.svg";
+import arrowRight from "../../assets/round-arrow-right.svg";
+
 const Connect = () => {
-  return <div>Connect</div>;
+  return (
+    <div className="connect-main">
+      <div className="connect-container">
+        <div className="top">
+          <p className="connect-p">
+            An enterprise template to ramp up your company website
+          </p>
+          <div className="connect-nav">
+            <img src={arrowLeft} alt="arrow" />
+            <img src={arrowRight} alt="arrow" />
+          </div>
+        </div>
+        <div className="bottom"></div>
+      </div>
+    </div>
+  );
 };
 
 export default Connect;
