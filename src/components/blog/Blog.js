c04c2d463550ecdd75eab48ec93c1cf1834bbd71
emmanuelolusola <emmanuelolusolaoyeleke@gmail.com>
2023-05-05 23:48:38 +0100

    blog section: updating

--- src/components/blog/Blog.js
+++ src/components/blog/Blog.js
@@ -1,7 +1,16 @@
 import React from "react";
 
 const Blog = () => {
-  return <div>Blog</div>;
+  return (
+    <div className="blog-main">
+      <div className="blog-container">
+        <p className="blog-p">Our blog</p>
+        <p className="blog-h">
+          Value proposition accelerator product management venture
+        </p>
+      </div>
+    </div>
+  );
 };
 
 export default Blog;
